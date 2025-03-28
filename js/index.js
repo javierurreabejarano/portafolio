8a1d646a458dfbb7709763416d1019c6b19b337a
javierurreabejarano <89669453+javierurreabejarano@users.noreply.github.com>
2025-03-28 14:52:47 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Obtener el año actual
+const añoActual = new Date().getFullYear();
+
+// Insertar el año actual en un elemento HTML
+document.getElementById("año-actual").textContent = añoActual;
